7095f979d02a0d4b0fe01246a6217245c112118f
Mukhammad Bobur Pakhriev <boburpakhriev@gmail.com>
2025-01-20 10:40:53 +0900

    Add js bundle generator, configure add multiple pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,31 @@
+const path = require("path");
+const fs = require("fs");
+
+const jsDir = path.resolve(__dirname, "./src/js");
+
+const generateEntryPoints = () => {
+  const files = fs.readdirSync(jsDir);
+  const entry = {};
+
+  files.forEach((file) => {
+    if (file.endsWith("js")) {
+      const name = path.basename(file, ".js");
+      entry[name] = path.join(jsDir, file);
+    }
+  });
+
+  return entry;
+};
+
 const config = {
   mode: "production",
-  entry: {
-    index: "./src/js/index.js",
-  },
+  entry: generateEntryPoints(),
+
+  // PERF: for a reference
+  // entry: {
+  //   index: "./src/js/index.js",
+  //   contacts: "./src/js/contacts.js",
+  // },
   output: {
     filename: "[name].bundle.js",
   },
