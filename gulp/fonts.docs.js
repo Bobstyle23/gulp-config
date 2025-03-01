0a159fd1499ecf5b08198e3d4b69cd2a6d7163ee
Mukhammad Bobur Pakhriev <boburpakhriev@gmail.com>
2025-03-01 11:02:19 +0900

    Update fonts styling config

--- gulp/fonts.docs.js
+++ gulp/fonts.docs.js
@@ -6,6 +6,8 @@ const gulp = require("gulp");
 const fonter = require("gulp-fonter-fix");
 const ttf2woff2 = require("gulp-ttf2woff2");
 
+const { getFontWeight } = require("../utilities.js");
+
 const srcFolder = "./src";
 const destFolder = "./docs";
 
@@ -50,56 +52,50 @@ gulp.task("ttfToWoff", () => {
     );
 });
 
-gulp.task("fontsStyle", () => {
+gulp.task("fontsStyle", (done) => {
   let fontsFile = `${srcFolder}/styles/scss/fonts/_fonts.scss`;
-  fs.readdir(`${destFolder}/fonts/`, function (err, fontsFiles) {
-    if (fontsFiles) {
-      fs.writeFile(fontsFile, "", cb);
-      let newFileOnly;
-      for (var i = 0; i < fontsFiles.length; i++) {
-        let fontFileName = fontsFiles[i].split(".")[0];
-        if (newFileOnly !== fontFileName) {
-          let fontName = fontFileName.split("-")[0]
-            ? fontFileName.split("-")[0]
-            : fontFileName;
-          let fontWeight = fontFileName.split("-")[1]
-            ? fontFileName.split("-")[1]
-            : fontFileName;
-          if (fontWeight.toLowerCase() === "thin") {
-            fontWeight = 100;
-          } else if (fontWeight.toLowerCase() === "extralight") {
-            fontWeight = 200;
-          } else if (fontWeight.toLowerCase() === "light") {
-            fontWeight = 300;
-          } else if (fontWeight.toLowerCase() === "medium") {
-            fontWeight = 500;
-          } else if (fontWeight.toLowerCase() === "semibold") {
-            fontWeight = 600;
-          } else if (fontWeight.toLowerCase() === "bold") {
-            fontWeight = 700;
-          } else if (
-            fontWeight.toLowerCase() === "extrabold" ||
-            fontWeight.toLowerCase() === "heavy"
-          ) {
-            fontWeight = 800;
-          } else if (fontWeight.toLowerCase() === "black") {
-            fontWeight = 900;
-          } else {
-            fontWeight = 400;
-          }
-          fs.appendFile(
-            fontsFile,
-            `@font-face {\n\tfont-family: ${fontName};\n\tfont-display: swap;\n\tsrc: url("../fonts/${fontFileName}.woff2") format("woff2"), url("../fonts/${fontFileName}.woff") format("woff");\n\tfont-weight: ${fontWeight};\n\tfont-style: normal;\n}\r\n`,
-            cb,
+  let fontsDir = `${destFolder}/fonts/`;
+
+  fs.readdir(fontsDir, (err, fontsFiles) => {
+    if (err) {
+      console.error("Error reading fonts directory:", err);
+      return done();
+    }
+
+    if (fontsFiles && fontsFiles.length > 0) {
+      let fontStyles = [];
+      let processedFonts = new Set();
+
+      fontsFiles.forEach((file) => {
+        let fontFileName = file.split(".")[0];
+        if (!processedFonts.has(fontFileName)) {
+          processedFonts.add(fontFileName);
+
+          let [fontName, fontWeight] = fontFileName.split("-");
+          fontName = fontName || fontFileName;
+          fontWeight = getFontWeight(fontWeight);
+
+          fontStyles.push(
+            `@font-face {
+  font-family: "${fontName}";
+  font-display: swap;
+  src: url("../fonts/${fontFileName}.woff2") format("woff2"),
+       url("../fonts/${fontFileName}.woff") format("woff");
+  font-weight: ${fontWeight};
+  font-style: normal;
+}`,
           );
-          newFileOnly = fontFileName;
         }
-      }
+      });
+
+      fs.writeFile(fontsFile, fontStyles.join("\n\n"), (err) => {
+        if (err) console.error("Error writing font styles:", err);
+        done();
+      });
+    } else {
+      done();
     }
   });
-
-  return gulp.src(`${srcFolder}`);
-  function cb() {}
 });
 
 gulp.task("fonts.docs", gulp.series("otfToTtf", "ttfToWoff", "fontsStyle"));
--- utilities.js
+++ utilities.js
@@ -30,4 +30,20 @@ const svgSymbol = {
   },
 };
 
-export { svgSymbol, svgStack };
+const getFontWeight = (weight) => {
+  const weightMap = {
+    thin: 100,
+    extralight: 200,
+    light: 300,
+    medium: 500,
+    semibold: 600,
+    bold: 700,
+    extrabold: 800,
+    heavy: 800,
+    black: 900,
+  };
+
+  return weightMap[weight?.toLowerCase()] || 400;
+};
+
+export { svgSymbol, svgStack, getFontWeight };
